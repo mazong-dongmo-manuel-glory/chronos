5b4dade43ddb5c88979b5973aca62c3b444bbf41
mazongdongmomanuelglory <mazongodngmomanuelglory@gmail.com>
2024-03-03 17:22:47 -0500

    starting

---
+++ crawler.py
@@ -0,0 +1,213 @@
+import requests 
+from urllib.parse import urlparse 
+from bs4 import BeautifulSoup 
+import threading
+import re 
+from mongoengine.connection import connect
+from db import Page,UrlFile
+import time
+import validators
+connect("search")
+class Data:
+    QUEUE = []
+    LOCK = threading.Lock()
+    IN_PROCESS = dict()
+    QUEUE_SIZE = 1000
+    QUEUE_SIZE_INTER = 10
+    NUMBER_OF_THREAD = 0
+class Crawler:
+
+    def __init__(self) -> None:
+        Data.LOCK.acquire()
+        Data.NUMBER_OF_THREAD += 1 
+        Data.LOCK.release()
+        pass
+    
+    def addToQueue(self,url):
+        Data.LOCK.acquire()
+        if type(url) == list:
+            Data.QUEUE =  Data.QUEUE + url
+        else:
+            Data.QUEUE.append(url)
+        Data.LOCK.release()
+        
+    def getToQueue(self):
+        Data.LOCK.acquire()
+        if len(Data.QUEUE)<=0:
+            url = None
+        else:
+            url = Data.QUEUE.pop(0)
+        
+        
+        Data.LOCK.release()
+        return url
+    def getInProcess(self,url):
+        Data.LOCK.acquire()
+        etat = url in Data.IN_PROCESS 
+        Data.LOCK.release()
+        return etat
+    def addInProcess(self,url):
+        Data.LOCK.acquire()
+        Data.IN_PROCESS[url] = time.time()
+        Data.LOCK.release()
+    def removeInProcess(self,url):
+        Data.LOCK.acquire()
+        try:
+            del Data.IN_PROCESS[url]
+        except:
+            pass
+        Data.LOCK.release()
+    
+    def addNumberOfThread(self):
+        Data.LOCK.acquire()
+        Data.NUMBER_OF_THREAD += 1 
+        Data.LOCK.release()
+    
+    def minusNumberOfThread(self):
+        Data.LOCK.acquire()
+        Data.NUMBER_OF_THREAD -= 1 
+        Data.LOCK.release()
+    
+    def crawl(self,root_url):
+        self.addToQueue(root_url)
+        while len(Data.QUEUE)>0:
+            url = self.getToQueue()
+            if "google" in url:
+                continue
+            if not self.getInProcess(url) and Data.NUMBER_OF_THREAD < 1000:
+                self.addInProcess(url)
+                cr = Crawler()
+                th = threading.Thread(target=cr.crawl,args=(url,))
+                th.start()
+                self.addNumberOfThread()
+                continue
+
+            #on verifie si la page existe deja
+            if Page.get(url):
+                self.removeInProcess(url)
+                continue
+            page = self.getPage(url)
+            if  not page:
+                self.removeInProcess(url)
+                continue
+            self.addToQueue(page["links"])
+            if Page.insert(page["url"],page["content"],page["links"]):
+                print(url)
+                pass
+            self.removeInProcess(url)
+        self.removeInProcess(url)
+        self.minusNumberOfThread()
+        
+            
+            
+    @classmethod
+    def handleQueue(cls):
+        def handle():
+            while True:
+                if len(Data.QUEUE) > Data.QUEUE_SIZE:
+                    Data.LOCK.acquire()
+                    to_save = Data.QUEUE[:len(Data.QUEUE)-Data.QUEUE_SIZE_INTER]
+                    Data.QUEUE = Data.QUEUE[len(Data.QUEUE)-Data.QUEUE_SIZE_INTER:]
+                    Data.LOCK.release()
+                
+                    for link in to_save:
+                        UrlFile.insert(link)
+                    
+                if len(Data.QUEUE) < Data.QUEUE_SIZE:
+                    cls.insertUrlInQueue()
+                time.sleep(3)
+                
+        threading.Thread(target=handle).start()
+    @classmethod
+    def insertUrlInQueue(cls):
+            Data.LOCK.acquire()
+            Data.QUEUE += UrlFile.get(Data.QUEUE_SIZE)
+            Data.LOCK.release()
+        
+        
+    def validate_domain(self,domain):
+        pattern = re.compile(
+        r'^(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]$')
+        return bool(pattern.match(domain))
+    
+    def validate_path(self,path):
+        pattern = re.compile(
+        r'^(/[^/ ]*)+/?$'
+        )
+        return bool(pattern.match(path))
+       #retourne tous les liens et le contenu de la page
+    def getPage(self,url:str)->bool|dict:
+        response = None
+        page  =  None
+        try:
+            response = requests.get(url)
+            if response.status_code != 200 or not response.headers["Content-Type"].startswith("text/html"):
+                return False 
+        except Exception as e:
+            return False
+        try:
+            page = BeautifulSoup(response.content,"html.parser")
+        except Exception as e:
+            return False 
+        links = set()
+        for link in page.find_all("a"):
+            link = urlparse(link.get("href"))
+            if not link.geturl():
+                continue
+            if link.netloc and link.netloc != "" and link.scheme and link.scheme != "":
+                links.add(link.geturl())
+                continue
+            elif not link.scheme and self.validate_domain(link.geturl()) and validators.url("https://"+link.geturl()):
+                links.add("https://"+link.geturl())
+                continue 
+            elif self.validate_path(link.geturl()):
+                if url.endswith("/") and link.geturl().startswith("/"):
+                    if validators.url(url[:len(url)-1]+link.geturl()):
+                        links.add(url[:len(url)-1]+link.geturl())
+                    continue
+                else:
+                    if validators.url(links.add(url+link.geturl())):
+                        links.add(links.add(url+link.geturl()))
+                    
+                continue
+            elif link.query.startswith("?") and validators.url(url+link.query):
+                links.add(url+link.query)
+                continue
+        content = ""
+        last_word = ""
+        for word in page.get_text().split():
+            if last_word != " ":
+                content = content + " " + word 
+            else:
+                content += word 
+            last_word = word 
+            
+        return {"url":url,"links":list(links),"content":content}
+    
+cr = Crawler()
+sites_science_actualite = [
+    "https://www.futura-sciences.com",
+    "https://www.sciencesetavenir.fr",
+    "https://www.lemonde.fr/sciences/",
+    "https://www.larecherche.fr",
+    "https://www.pourlascience.fr",
+    "https://www.science-et-vie.com",
+    "https://www.cnrs.fr",
+    "https://www.inserm.fr",
+    "https://www.cea.fr",
+    "https://www.techno-science.net",
+    "https://www.scienceinfo.fr",
+    "https://www.sciencepresse.qc.ca",
+    "https://www.sciences-faits-histoires.com",
+    "https://www.afis.org",
+    "https://www.universcience.fr",
+    "https://www.espace-sciences.org",
+    "https://www.palais-decouverte.fr",
+    "https://www.cite-sciences.fr",
+    "https://www.cap-sciences.net",
+    "https://www.lacitedes-sciences.fr"
+]
+Data.QUEUE += sites_science_actualite
+Crawler.handleQueue()
+
+cr.crawl("https://salvatore.com/fr/")
---
+++ db.py
@@ -0,0 +1,66 @@
+from mongoengine.fields import URLField,StringField,ListField,DateField
+from mongoengine.document import Document
+from datetime import datetime
+class UrlFile(Document):
+    url = URLField(unique=True)
+    timestamp = DateField(default=datetime.now())
+    
+    @classmethod 
+    def insert(cls,url):
+        url = UrlFile(url=url)
+
+        try:
+            url.validate()
+            url.save()
+            return True
+        except Exception as e:
+            return False
+    @classmethod 
+    def get(cls,limit=20):
+        urls = None
+        try:
+            urls = cls.objects.order_by('timestamp').limit(limit)
+            if len(urls) == 0:
+                return []
+        except:
+            return []
+        result = [url.url for url in urls]
+        for url in urls:
+            try:
+                url.delete()
+            except Exception as e:
+                print(f"Failed to delete url ")
+        
+        return result
+
+class Page(Document):
+    url = URLField(unique=True)
+    timestamp = DateField(default=datetime.now())
+    links = ListField(URLField())
+    content = StringField()
+    
+    @classmethod 
+    def insert(cls,url,content,links):
+        page = Page(url=url,content=content,links=links)
+        
+        try:
+            page.validate()
+        except Exception as e:
+            return False
+        try:
+            page.save()
+            return True
+        except Exception as e:
+            return False
+    
+    @classmethod 
+    def get(cls,url):
+        try:
+            page = Page.objects(url=url).first()
+            if page:
+                return True
+            return False
+        except:
+            return False
+            
+            
